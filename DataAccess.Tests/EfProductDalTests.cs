1f14efe8d8ebac7a2720c01950c108bdb2a950e3
Nafiye Demiroğ <nafiye.demirog@gmail.com>
2020-12-17 18:54:57 +0300

    Katmanlar oluşturuldu

---
+++ DataAccess.Tests/EfProductDalTests.cs
@@ -0,0 +1,21 @@
+﻿using DataAccess.Concrete.EntityFramework;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DataAccess.Tests
+{
+    [TestClass]
+    public class EfProductDalTests
+    {
+        [TestMethod]
+        public void TestGetAll()
+        {
+            EfProductDal efProductDal = new EfProductDal();
+            Assert.IsTrue(efProductDal.GetList().Any());
+            Assert.AreEqual(77, efProductDal.GetList().Count);
+        }
+    }
+}
