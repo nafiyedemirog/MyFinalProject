21d4bda634af2c132291f17eed9ea8668b889d26
Nafiye Demiroğ <nafiye.demirog@gmail.com>
2020-12-18 09:51:56 +0300

    Category eklendi

---
+++ DataAccess.Tests/EfCategoryDalTests.cs
@@ -0,0 +1,20 @@
+﻿using DataAccess.Concrete.EntityFramework;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DataAccess.Tests
+{
+    [TestClass]
+    public class EfCategoryDalTests
+    {
+        [TestMethod]
+        public void TestGetAll()
+        {
+            EfCategoryDal efCategoryDal = new EfCategoryDal();
+            Assert.IsTrue(efCategoryDal.GetList().Any());
+        }
+    }
+}
